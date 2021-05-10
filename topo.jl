2d3f011eef97b0194e737c9e0d7f85d394cab9a2
mahmoud-al-najar <mahmoud.najar.95@gmail.com>
2021-05-10 13:41:46 +0200

    data loading, eval, mae fitness

--- topo.jl
+++ topo.jl
@@ -9,14 +9,19 @@ using Plots
 include("utils.jl")
 
 
+struct Month
+    topo0
+    topo1
+    tp
+    hs
+    direction
+end
+
 dataset_path = get_dataset_path()
-dataset_size = 236
+n_months = 236
+months = []
 
-X = zeros(748, dataset_size)
-Y = zeros(76, dataset_size)
-
-for i in 1:dataset_size
-    global X
+for i in 1:n_months
     topo_path = string(dataset_path, '/', i, "_topo.npy")
     topo = npzread(topo_path)
     topo0 = norm(topo[1, :], -8.0, 8.0)
@@ -26,71 +31,44 @@ for i in 1:dataset_size
     waves_csv = CSV.read(waves_path, DataFrame)
 
     # t = min_max_norm(waves_csv.t)
-    tp = norm(waves_csv.tp, 0, 25)
-    hs = norm(waves_csv.hs, 0, 10)
-    direction = norm(waves_csv.direction, 0, 360)
+    tp_month = norm(waves_csv.tp, 0, 25)
+    hs_month = norm(waves_csv.hs, 0, 10)
+    direction_month = norm(waves_csv.direction, 0, 360)
 
-    x_row = vcat(topo0, tp, hs, direction)
-    y_row = topo1
-
-    X[:, i] = x_row
-    Y[:, i] = y_row
-end
-
-
-
-function pass_month(ind, month_x)
-    topo0 = month_x[1:76]
-    
-    shift = 76
-    tp_month = month_x[shift:shift+224]
-
-    shift = shift + 224
-    hs_month = month_x[shift:shift+224]
-
-    shift = shift + 224
-    direction_month = month_x[shift:shift+224]
-
-    out = []
+    tp_weeks = []
+    hs_weeks = []
+    direction_weeks = []
 
     for i in range(0, stop=27)
-        tp = tp_month[(i*8) + 1 : (i*8) + 8 + 1]
-        hs = hs_month[(i*8) + 1 : (i*8) + 8 + 1]
-        direction = direction_month[(i*8) + 1 : (i*8) + 8 + 1]
-
-        x_row = vcat(topo0, tp, hs, direction)
-        out = process(ind, x_row)
+        tp = tp_month[(i*8) + 1 : (i*8) + 8]
+        hs = hs_month[(i*8) + 1 : (i*8) + 8]
+        direction = direction_month[(i*8) + 1 : (i*8) + 8]
+        push!(tp_weeks, tp)
+        push!(hs_weeks, hs)
+        push!(direction_weeks, direction)
     end
-    out
+    month = Month(topo0, topo1, tp_weeks, hs_weeks, direction_weeks)
+    global months
+    push!(months, month)
 end
 
-function evaluate(ind::CGPInd, X::AbstractArray, Y::AbstractArray)
-    out = pass_month(ind, X[:, 1])
-    out = denorm(out, -8, +8)
-    
-    errors = out - Y[:, 1]
-    errors = errors .^ 2
-    mse = mean(errors)
-    mean_mse = mse
+function evaluate(ind::CGPInd, months::Vector{Any})    
+    errors = Vector{Float64}(undef, size(months)[1])
 
-    for i in 2:size(X, 2)
-        out = pass_month(ind, X[:, i])
-        out = denorm(out, -8, +8)
-        y = denorm(Y[:, i], -8, +8)
-        errors = out - y
-        errors = errors .^ 2
-        mse = mean(errors)
-        if abs(mse) == Inf || isnan(mse)
-            mse = 500
+    for m_i in 1:size(months)[1]
+        month = months[m_i]
+        topo0 = deepcopy(month.topo0)
+        for w_i in 1:size(month.tp)[1]
+            x_row = vcat(topo0, month.tp[w_i], month.hs[w_i], month.direction[w_i])
+            topo0 = process(ind, x_row)
         end
-        mean_mse = (mean_mse + mse) / 2.0
+        errors[m_i] = mean(abs.(month.topo1 - topo0))
     end
-    neg_rmse = sqrt(mean_mse) * -1
-    [neg_rmse]
+    [mean(errors) * -1]
 end
 
 cfg = get_config("topo.yaml")
-fit(i::CGPInd) = evaluate(i, X, Y)
+fit(i::CGPInd) = evaluate(i, months)
 mutate(i::CGPInd) = goldman_mutate(cfg, i)
 e = CGPEvolution(cfg, fit)
 run!(e)
