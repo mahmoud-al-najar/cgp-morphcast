8c5a95f611390240435a970d1d64de8312159bdd
mahmoud-al-najar <mahmoud.najar.95@gmail.com>
2021-05-12 10:44:37 +0200

    refactoring

--- utils.jl
+++ utils.jl
@@ -1,20 +1,10 @@
 using JSON
 
-function norm(arr, min, max)
-    (arr .- min) ./ (max - min)
-end
+get_dataset_path() = JSON.parsefile("paths.json")["dataset_path"]
 
-function denorm(arr, min, max)
-    arr .* (max - min) + min
-end
-
-function norm_to_range(arr, min_x, max_x, range_a, range_b)
-    (range_b - range_a) .* ((arr .- min_x) ./ (max_x - min_x)) .+ range_a
-end
-
-function get_dataset_path()
-    JSON.parsefile("paths.json")["dataset_path"]
-end
+norm(arr, min, max) = (arr .- min) ./ (max - min)
+denorm(arr, min, max) = arr .* (max - min) + min
+norm_to_range(arr, min_x, max_x, range_a, range_b) = (range_b - range_a) .* ((arr .- min_x) ./ (max_x - min_x)) .+ range_a
 
 has_inf(arr) = sum(isinf.(arr)) > 0
 has_nan(arr) = sum(isnan.(arr)) > 0
